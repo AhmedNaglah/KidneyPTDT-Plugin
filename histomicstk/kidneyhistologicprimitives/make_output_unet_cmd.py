01dfe3ca143c7ac013517c5d83a737a60408d5e2
AhmedNaglah <ahmed.naglah@gmail.com>
2024-02-24 18:51:05 -0500

    docker

--- histomicstk/kidneyhistologicprimitives/make_output_unet_cmd.py
+++ histomicstk/kidneyhistologicprimitives/make_output_unet_cmd.py
@@ -46,9 +46,13 @@ batch_size = args.batchsize
 patch_size = args.patchsize
 stride_size = patch_size//2
 
-
 # ----- load network
 device = torch.device(args.gpuid if torch.cuda.is_available() else 'cpu')
+print(device)
+
+device = torch.device('cpu')
+print(device)
+print(args.model)
 
 checkpoint = torch.load(args.model, map_location=lambda storage, loc: storage) #load checkpoint to CPU and then put to device https://discuss.pytorch.org/t/saving-and-loading-torch-models-on-2-machines-with-different-number-of-gpu-devices/6666
 model = UNet(n_classes=checkpoint["n_classes"], in_channels=checkpoint["in_channels"],
